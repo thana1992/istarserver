d352208aae3981c16345d75c140facf848371fad
thana_pha <thana_pha@freewillgroup.com>
2024-01-19 13:56:11 +0700

    up

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ app.use(cors({
 app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header(`Access-Control-Allow-Origin`, `192.168.1.81:8080`);
   // other headers...
   next();
 });
