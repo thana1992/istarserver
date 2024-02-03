35673e6aba90eea3ce333cc3a33113177c8c3b11
thana_pha <thana_pha@freewillsolutions.com>
2024-02-04 02:47:51 +0700

    Update totalStudents count to 999

--- server.js
+++ server.js
@@ -885,7 +885,7 @@ app.post('/register', async (req, res) => {
       const results1 = await queryPromise(query1);
       if (results1.length > 0) {
         datacard.totalStudents = results1[0].total;
-        datacard.totalStudents = 700;
+        datacard.totalStudents = 999;
       }
   
       // Query 2
