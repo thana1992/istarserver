d48ef6c3bf706af8095256e41afe00feffd61f29
thana1992 <thana.phalodom@gmail.com>
2024-05-01 19:21:58 +0700

    Update mysql2 package version to 3.9.7 and add qs package

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const axios = require('axios');
+const qs = require('qs');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
