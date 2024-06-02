b561b8d8fc952f4305da5470b05da313e3587880
thana_pha <thana_pha@freewillsolutions.com>
2024-06-03 00:24:42 +0700

    Refactor server.js to update courserefer parameter in addStudentByAdmin and updateStudentByAdmin APIs

--- server.js
+++ server.js
@@ -303,8 +303,8 @@ app.post('/addStudentByAdmin', verifyToken, async (req, res) => {
       } else {
         const coursetype = resCheckCustomerCourse[0].coursetype;
         if(coursetype == 'Monthly') {
-          const queryCheckUserd = 'SELECT count(*) FROM tstudent WHERE courserefer = ? AND studentid <> ?';
-          const resCheckUserd = await queryPromise(queryCheckUserd, [courserefer, studentid]);
+          const queryCheckUserd = 'SELECT count(*) FROM tstudent WHERE courserefer = ? ';
+          const resCheckUserd = await queryPromise(queryCheckUserd, [courserefer]);
           if (resCheckUserd.length > 0) {
             const count = resCheckUserd[0].count;
             if (count > 0) {
