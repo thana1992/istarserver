adb2f83aae65cf3d588aa09320a7c02e32c6c9e8
thana_pha <thana_pha@freewillsolutions.com>
2024-07-08 16:31:26 +0700

    Refactor server.js to set the server's time zone to 'Asia/Bangkok'

--- server.js
+++ server.js
@@ -1716,6 +1716,8 @@ app.listen(port, '0.0.0.0', () => {
   clearActiveSessions();
   console.log(`Server is running on port ${port}`);
   console.log(" Start time : " + timestamp)
+  const query = "SET time_zone = 'Asia/Bangkok'";
+  queryPromise(query, null);
 });
 
 // ทำให้ console.log ใช้ winston logger
