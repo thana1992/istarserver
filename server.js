0bbdb06fdbea178d97e39835a899c26c90e95051
thana_pha <thana_pha@freewillsolutions.com>
2024-08-01 04:15:47 +0700

    refactor: Update variable name for course short name in server.js
    This commit updates the variable name `cours_shortename` to `course_shortename` in multiple API endpoints (`addBookingByAdmin`, `updateBookingByAdmin`, `createReservation`) in the `server.js` file. This change ensures consistency and improves code readability.

--- server.js
+++ server.js
@@ -1615,7 +1615,6 @@ app.get('/getStudentCourseDetail/:courserefer', verifyToken, async (req, res) =>
       CASE WHEN cc.coursetype = 'Monthly' THEN cc.coursetype ELSE cc.remaining END 'remaining', cc.expiredate 
     FROM tcustomer_course cc 
     LEFT JOIN tstudent s ON cc.courserefer = s.courserefer 
-    GROUP BY cc.courserefer, cc.coursetype, cc.remaining, cc.expiredate;
     `;
 
     let queryParams = [];
