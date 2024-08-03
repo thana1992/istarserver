cf874f25a06a46903cc308d848d2ffd040b49a93
thana_pha <thana_pha@freewillsolutions.com>
2024-08-04 02:47:07 +0700

    refactor: Update JOIN condition in server.js to include enable flag check
    This commit updates the JOIN condition in the server.js file to include a check for the enable flag in the tcourseinfo table. Previously, the JOIN was only based on the courseid, but now it also ensures that the enableflag is set to 1. This change ensures that only enabled courses are included in the query results, improving data accuracy and consistency.

--- server.js
+++ server.js
@@ -1470,7 +1470,7 @@ app.post('/getBookingList', verifyToken, async (req, res) => {
   console.log("getBookingList [request] : " + JSON.stringify(req.body));
   try {
     const { classday, classdate } = req.body;
-    const query = 'SELECT DISTINCT a.classtime, a.courseid, CONCAT(a.classtime,\' (\',b.course_shortname,\')\') as class_label, a.classid FROM tclassinfo a join tcourseinfo b on a.courseid = b.courseid and b.enableflag = 1 where a.classday = ? order by a.classtime'
+    const query = 'SELECT DISTINCT a.classtime, a.courseid, CONCAT(a.classtime,\' (\',b.course_shortname,\')\') as class_label, a.classid FROM tclassinfo a join tcourseinfo b on a.courseid = b.courseid and b.enableflag = 1 where a.classday = ? and a.enableflag = 1 order by a.classtime'
     const results = await queryPromise(query, [classday]);
     console.log("results : " + JSON.stringify(results));
     let bookinglist = {};
