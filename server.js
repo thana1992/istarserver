82d5ec6b3d48d242ce2647bf63b83b95c8f5daeb
thana_pha <thana_pha@freewillsolutions.com>
2024-11-23 00:46:39 +0700

    Refactor: Simplify SQL query in checkTimeZone function for improved readability

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ const pool = mysql2.createPool({
 
 async function checkTimeZone() {
   try {
-    const checkVar1 = await queryPromise("select CURTIME() as curtime, CURRENT_TIME() as current_time , CURRENT_TIMESTAMP() as timestamp, NOW() as now", [], true);
+    const checkVar1 = await queryPromise("select CURTIME(), CURRENT_TIME() , CURRENT_TIMESTAMP() , NOW() ", [], true);
     console.log('Check time zone : ', stringify(checkVar1[0]));
     
   } catch (error) {
