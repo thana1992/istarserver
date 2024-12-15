04745d06e8bb23ac9bd7d1cd54df74e0e8f66632
thana1992 <thana.phalodom@gmail.com>
2024-12-15 18:06:12 +0700

    Refactor: Comment out payment status logic in booking list response for clarity and update age calculation to return decimal format

--- server.js
+++ server.js
@@ -1729,29 +1729,29 @@ app.post('/getBookingList', verifyToken, async (req, res) => {
 
       if (nickname) {
         if (row.checkedin == 1 && row.color != null) {
-          if(isExpired(row.expiredate) || row.remaining == 0) {
-            acc[classLabel].push(`${nickname}(pay)(${row.checkedin})(${row.color})`);
-          }else{
+          // if(isExpired(row.expiredate) || row.remaining == 0) {
+          //   acc[classLabel].push(`${nickname}(pay)(${row.checkedin})(${row.color})`);
+          // }else{
             acc[classLabel].push(`${nickname}(${row.checkedin})(${row.color})`);
-          }
+          //}
         } else if (row.checkedin == 1) {
-          if(isExpired(row.expiredate) || row.remaining == 0) {
-            acc[classLabel].push(`${nickname}(pay)(${row.checkedin})`);
-          }else{
+          // if(isExpired(row.expiredate) || row.remaining == 0) {
+          //   acc[classLabel].push(`${nickname}(pay)(${row.checkedin})`);
+          // }else{
             acc[classLabel].push(`${nickname}(${row.checkedin})`);
-          }
+          //}
         } else if (row.color != null) {
-          if(isExpired(row.expiredate) || row.remaining == 0) {
-            acc[classLabel].push(`${nickname}(pay)(${row.color})`);
-          }else{
+          // if(isExpired(row.expiredate) || row.remaining == 0) {
+          //   acc[classLabel].push(`${nickname}(pay)(${row.color})`);
+          // }else{
             acc[classLabel].push(`${nickname}(${row.color})`);
-         }
+         // }
         } else {
-          if(isExpired(row.expiredate) || row.remaining == 0) {
-            acc[classLabel].push(`${nickname}(pay)`);
-          }else{
+          // if(isExpired(row.expiredate) || row.remaining == 0) {
+          //   acc[classLabel].push(`${acc[classLabel]}(pay)`);
+          // }else{
             acc[classLabel].push(nickname);
-          }
+          //}
         }
         
       }
@@ -1793,7 +1793,8 @@ function calculateAge(dateOfBirth) {
   const ageDate = new Date(diff);
   const ageYears = ageDate.getUTCFullYear() - 1970;
   const ageMonths = ageDate.getUTCMonth();
-  return `${ageYears} ปี ${ageMonths} เดือน`;
+  const ageDecimal = ageYears + (ageMonths / 12);
+  return ageDecimal.toFixed(2); // แสดงผลเป็นทศนิยม 2 ตำแหน่ง
 }
 
 app.post('/getCustomerCourseList', verifyToken, async (req, res) => {
