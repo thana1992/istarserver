1d557795451a1fd1db0178f5422fbef74101c3cf
thana1992 <thana.phalodom@gmail.com>
2025-04-05 17:03:35 +0700

    Feature: Add error handling for Discord webhook and log course actions

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const jwt = require('jsonwebtoken');
 const SECRET_KEY = process.env.SECRET_KEY;
 const activeSessions = [];
 const blacklistSessions = [];
-const { logSystemToDiscord, logBookingToDiscord, sendNotification, sendNotificationUpdate } = require('./logToDiscord'); // import function ที่แยกไว้
+const { logSystemToDiscord, logBookingToDiscord, sendNotification, sendNotificationUpdate, logCourseToDiscord } = require('./logToDiscord'); // import function ที่แยกไว้
 const mysql2 = require('mysql2/promise');
 const { stringify } = require('querystring');
 
