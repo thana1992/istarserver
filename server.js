9d8d010f3b933fea29f6b405318a3ddb63113e9a
thana1992 <thana.phalodom@gmail.com>
2025-04-06 15:21:43 +0700

    Feature: Comment out logToQueue call in API response logging for debugging purposes

--- server.js
+++ server.js
@@ -173,7 +173,7 @@ app.use((req, res, next) => {
     logger.info(`-----> RESPONSE : ${req.url} : ---> ${logBody}`);
     const username = req.user && req.user.username ? req.user.username : 'Unknown User';
     const timestamp = new Date().toISOString();
-    logToQueue('apicall', `[${timestamp}] [${username}] Request[${req.method}] ${req.url}`);
+    //logToQueue('apicall', `[${timestamp}] [${username}] Request[${req.method}] ${req.url}`);
     // Send the original body to the client
     originalSend.call(res, body);
   };
